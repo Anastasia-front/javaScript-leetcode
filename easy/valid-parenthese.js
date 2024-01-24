d99cb062c7c68f6afdd9b9a255f15c0f1bab50c6
Anastasia-front <palitsanastasia3.ap@gmail.com>
2024-01-24 18:52:14 +0200

    Create valid-parenthese.js

---
+++ easy/valid-parenthese.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  const obj = { "(": ")", "{": "}", "[": "]" };
+  const array = [];
+
+  if (s.length % 2 !== 0) {
+    return false;
+  }
+
+  for (const bracket of s) {
+    if (bracket in obj) {
+      array.push(bracket);
+    } else if (array.length === 0 || obj[array.pop()] !== bracket) {
+      return false;
+    }
+  }
+
+  return array.length === 0;
+};
+
+// Runtime 59 ms / Memory 49.84 mb
+console.log(isValid("()[]{}"));
+console.log(isValid("(]{}"));
+console.log(isValid("[}"));
